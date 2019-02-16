eefbf1bfb31a4c8d3fc8f4f7adda0c6edb7c439f
Allef Sousa <allefsousa_1@hotmail.com>
2019-02-16 13:24:51 -0200

    Criando Entidade e Salvando dados

--- CoreData-IOS/ViewController.swift
+++ CoreData-IOS/ViewController.swift
@@ -7,13 +7,42 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        // Fazendo refencia a classe app delegate que contem os metodos para manipular o banco de dadaos (Core Data)
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        let aa  = UIApplication.shared.delegate as! AppDelegate
+        let bb = aa.persistentContainer.viewContext
+        
+        /*
+         Criando Entidade
+         */
+        let user = NSEntityDescription.insertNewObject(forEntityName: "Usuario", into: bb)
+        _ = NSEntityDescription.insertNewObject(forEntityName: "Usuario",into:context)
+        
+        user.setValue("Bruna mendes", forKey: "nome")
+        user.setValue(23, forKey: "idade")
+        user.setValue("bruninhadorme@dorme.com", forKey: "login")
+        user.setValue("123455", forKey: "senha")
+        
+        //salvando
+        
+        do {
+            try context.save()
+            print("Os Dados Foram Salvos")
+
+        } catch  {
+            print("Erro ao salvar dados")
+        }
     }
+    
 
 
 }
