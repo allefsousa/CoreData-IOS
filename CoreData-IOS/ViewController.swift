a07861fc8c52f8097c64895cb0ce21e506297c3d
Allef Sousa <allefsousa_1@hotmail.com>
2019-02-16 12:40:16 -0200

    Inicio Projeto

---
+++ CoreData-IOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreData-IOS
+//
+//  Created by Allef Sousa Santos on 16/02/19.
+//  Copyright © 2019 Allef Sousa Santos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
